e574f33fa9422752f557358f86cd73995b68a095
Michael Allen <michael@allenhub.net>
2019-07-02 12:54:31 -0400

    closes:#7

--- section5/test_Checkout.py
+++ section5/test_Checkout.py
@@ -12,3 +12,12 @@ def test_CanCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
+
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
+
