6b1dd8c7aade6c6fb2528f2f8b01b1ce07ecc557
Michael Allen <michael@allenhub.net>
2019-07-02 13:29:43 -0400

    closes:#9

--- section5/test_Checkout.py
+++ section5/test_Checkout.py
@@ -5,18 +5,17 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 
 def test_CanCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 
 def test_GetCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice("a", 1)
-    checkout.addItemPrice("b", 2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3
@@ -24,3 +23,10 @@ def test_GetCorrectTotalWithMultipleItems(checkout):
 
 def test_cannAddDiscountRule(checkout):
     checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
+
+
+
