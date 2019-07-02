a4d577577dcc029f0c4a2ceff225aa44d248448b
Michael Allen <michael@allenhub.net>
2019-07-02 12:47:01 -0400

    closes:#5

--- section5/Checkout.py
+++ section5/Checkout.py
@@ -3,3 +3,8 @@ class Checkout:
     def addItemPrice(self, item, price):
         pass
 
+    def addItem(self, item):
+        pass
+
+
+
--- section5/test_Checkout.py
+++ section5/test_Checkout.py
@@ -1,6 +1,15 @@
+import pytest
 from Checkout import Checkout
 
-def test_CannAddItemPrice():
-    co = Checkout()
-    co.addItemPrice("a", 1)
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
+
+def test_CannAddItemPrice(checkout):
+    checkout.addItemPrice("a", 1)
+
+
+def test_CanAddItem(checkout):
+    checkout.addItem("a")
